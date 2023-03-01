f2361d3959456f0feceb238f36281d0ffaa38005
Taras.Kravets <taraskravets95@gmail.com>
2023-03-02 01:54:03 +0200

    feat(signUp): added connection to mongoDB and feat-signup

--- src/Components/Auth/AuthForm.js
+++ src/Components/Auth/AuthForm.js
@@ -1,24 +1,70 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import styles from '@/styles/Auth/AuthForm.module.css';
+
+async function createUser(email, password) {
+	const response = await fetch('/api/auth/signup/', {
+		method: 'POST',
+		body: JSON.stringify({
+			email: email,
+			password: password,
+		}),
+		headers: {
+			'content-type': 'application/json',
+		},
+	});
+
+	const data = await response.json();
+
+	if (!response.ok) {
+		throw new Error(data.message || 'something went wrong');
+	}
+
+	return data;
+}
 
 function AuthForm() {
 	const [isLogin, setIsLogin] = useState(true);
+
+	const inputEmailRef = useRef();
+	const inputPasswordRef = useRef();
 
 	function switchAuthModeHandler() {
 		setIsLogin((prevState) => !prevState);
 	}
 
+	async function submitHandler(event) {
+		event.preventDefault();
+
+		const email = inputEmailRef?.current.value;
+		const password = inputPasswordRef?.current.value;
+
+		if (isLogin) {
+		} else {
+			try {
+				const result = createUser(email, password);
+				console.log(result);
+			} catch (error) {
+				console.error(error.message);
+			}
+		}
+	}
+
 	return (
 		<section className={styles.auth}>
 			<h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-			<form>
+			<form onSubmit={submitHandler}>
 				<div className={styles.control}>
 					<label htmlFor='email'>Your Email</label>
-					<input type='email' id='email' required />
+					<input type='email' id='email' required ref={inputEmailRef} />
 				</div>
 				<div className={styles.control}>
 					<label htmlFor='password'>Your Password</label>
-					<input type='password' id='password' required />
+					<input
+						type='password'
+						id='password'
+						required
+						ref={inputPasswordRef}
+					/>
 				</div>
 				<div className={styles.actions}>
 					<button>{isLogin ? 'Login' : 'Create Account'}</button>
